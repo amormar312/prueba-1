8c314559e18f7403d126a6f191b5258ad269473b
amormar <amormar312@g.educaand.es>
2023-10-04 10:35:22 +0200

    prueba 1

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -4,12 +4,12 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Escribe 3 numero");
-        int a, b, c, rb, rc;
+        double a, b, c, rb, rc;
         a= sc.nextInt();
         b= sc.nextInt();
         c= sc.nextInt();
-        rb=((2+a*b)/4)^(c+2);
-        rc= (-b + (b^2-4*a*c)^(1/2))/2*a;
+        rb=Math.pow((2+a*b)/4,(c+2));
+        rc= (-b +Math.sqrt(Math.pow(b, 2)-4*a*c))/2*a;
         System.out.println(a*b+c*((3-a)/2*b) + " Resultado a");
         System.out.println(rb + " Resultado b");
         System.out.println(rc + " Resultado c");
